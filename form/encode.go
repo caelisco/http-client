0c9229617219531789ed721f8fb3ded45e8c9932
James Lawson <consulting@caelisco.net>
2025-01-08 09:25:38 +1100

    form encoding comments

--- form/encode.go
+++ form/encode.go
@@ -5,8 +5,10 @@ import (
 	"strings"
 )
 
+// Encode encodes the provided map[string]string as application/x-www-form-urlencoded form data.
+// Each key and value in the map is URL-encoded and concatenated into a query string format.
+// The resulting query string is returned as a byte slice, suitable for use as the body of a POST request.
 func Encode(m map[string]string) []byte {
-	// Build the form data query string
 	var encoded []string
 	for k, v := range m {
 		key := url.QueryEscape(k)
